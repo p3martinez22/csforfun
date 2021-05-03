9591d39e6f691407f4085d60f59bb11b7f2fb543
mmartinez22522 <83504947+mmartinez22522@users.noreply.github.com>
2021-05-03 15:28:11 -0700

    Add files via upload

---
+++ gclicker.js
@@ -0,0 +1,27 @@
+var score = 0;
+var numsenor = 0;
+function buy(c)
+{
+if(c == 'senor')
+{
+    if(score > 49)
+    {
+        score = score - 50;
+        document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
+        numsenor++;
+        autosenor();
+    }
+}
+}
+function umclick()
+{
+    score++;
+    //alert(score);
+    document.getElementById("uscore").innerHTML = "Tienes " + score + " galletas";
+}
+function autosenor();
+{
+    var point = numsenor*10;
+    score = score + point;
+    setTimeout(autosenor(),10000);
+}
